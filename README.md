a890bf709374d0b5528c39aa0e44418309af0b1d
NepoleonAsus <835410808@qq.com>
2018-04-09 20:53:50 +0800

    2018-4-9

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 ## 2018-4-7 
 [Jewels and Stones](https://leetcode.com/problems/jewels-and-stones/description//)  
 [Judge Route Circle](https://leetcode.com/problems/judge-route-circle/description/)  
-[Unique Morse Code Words](https://leetcode.com/problems/unique-morse-code-words/description/)  
+[Unique Morse Code Words](https://leetcode.com/problems/unique-morse-code-words/description/)    
+## 2018-4-9  
+[Median of Two Sorted Arrays](https://leetcode.com/problems/median-of-two-sorted-arrays/description/)  
+[Merge Two Binary Trees](https://leetcode.com/problems/merge-two-binary-trees/description/)
--- Solution.java
+++ Solution.java
@@ -1,9 +1,19 @@
 package leetcoede;
 
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+
 public class Solution {
+	// Definition for a binary tree node.
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode(int x) { val = x; }
+	}
+	
 	public int numJewelsInStones(String J, String S) {
 		int num = 0;
 		for(int i = 0; i < J.length(); i++) {
@@ -54,4 +64,54 @@ public class Solution {
 		}
         return reSet.size();
     }
+
+	public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+		int m = nums1.length;
+		int n = nums2.length;
+		int[] nums = new int[m+n];
+		int i = 0;
+		int j = 0;
+		int k = 0;
+		while (i < m && j < n) {
+			if(nums1[i] < nums2[j])
+			{
+				nums[k++] = nums1[i];
+				i++;
+			}
+			else {
+				nums[k++] = nums2[j];
+				j++;
+			}
+		}
+		while (i < m) {
+			nums[k++] = nums1[i];
+			i++;
+		}
+		while (j < n) {
+			nums[k++] = nums2[j];
+			j++;
+		}
+		int index1 = 0;
+		int index2 = 0;
+		
+		if((m+n)%2==0) {
+			index1 = (m+n)/2-1;
+			index2 = (m+n)/2;
+		}
+		else {
+			index1 = index2 = (m+n)/2;
+		}
+		return (nums[index1]+nums[index2])/2.0;
+	}
+	
+	public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+		if (t1 == null)
+            return t2;
+        if (t2 == null)
+            return t1;
+        t1.val += t2.val;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+	}
 }
