7d5068b78db3bb3c9b8afaf6925edb2d9760e307
NepoleonAsus <835410808@qq.com>
2018-08-02 20:19:12 +0800

    剑指offer 二维数组的查找

---
+++ src/nowcoder/searchInTwoDimensionalArray.java
@@ -0,0 +1,34 @@
+package nowcoder;
+
+public class searchInTwoDimensionalArray {
+	public boolean Find(int target, int[][] array) {
+		if (array == null) {
+			return false;
+		}
+		int rows = array.length;
+		int cols = array[0].length;
+
+		int row = 0;
+		int col = cols - 1;
+
+		while (row < rows && col > 0) {
+			if (array[row][col] == target) {
+				return true;
+			} else if (array[row][col] > target) {
+				col--;
+			} else {
+				row++;
+			}
+		}
+		
+		return false;
+	}
+
+	public static void main(String[] args) {
+		int[][] array = { { 1, 2, 8, 9 }, { 2, 4, 9, 12 }, { 4, 7, 10, 13 }, { 6, 8, 11, 15 } };
+		searchInTwoDimensionalArray searchInTwoDimensionalArray = new searchInTwoDimensionalArray();
+
+		boolean res = searchInTwoDimensionalArray.Find(1, array);
+		System.out.println(res);
+	}
+}
