e739385f3d4906779aae90cce952d92f00fb66ef
NepoleonAsus <835410808@qq.com>
2018-08-05 20:57:25 +0800

    剑指offer 矩形覆盖

---
+++ src/nowcoder/RectCover.java
@@ -0,0 +1,16 @@
+package nowcoder;
+
+public class RectCover {
+	public int RectCover(int target) {
+		if (target <= 2) {
+			return target;
+		}
+		return RectCover(target - 1) + RectCover(target - 2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
