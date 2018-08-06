492e1eb6ab045c2b9c87a243a3654795566c5253
scy <835410808@qq.com>
2018-08-06 09:08:47 +0800

    剑指offer 变态跳台阶

---
+++ src/nowcoder/JumpFloorII.java
@@ -0,0 +1,13 @@
+package nowcoder;
+
+public class JumpFloorII {
+	public int JumpFloorII(int target) {
+		if (target <= 0) {
+			return -1;
+		} else if (target == 1) {
+			return 1;
+		} else {
+			return 2 * JumpFloorII(target - 1);
+		}
+	}
+}
