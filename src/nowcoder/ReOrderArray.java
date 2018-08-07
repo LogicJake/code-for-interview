925f7dee0aa46306fc7b2ebc478664aa7fd82af9
scy <835410808@qq.com>
2018-08-07 16:05:15 +0800

    剑指offer 调整数组顺序使奇数位于偶数前面

---
+++ src/nowcoder/ReOrderArray.java
@@ -0,0 +1,29 @@
+package nowcoder;
+
+public class ReOrderArray {
+	public static void reOrderArray(int[] array) {
+		int[] newArray = new int[array.length];
+		int index = 0;
+		for (int i : array) {
+			if (i % 2 == 1) {
+				newArray[index++] = i;
+			}
+		}
+		for (int i : array) {
+			if (i % 2 == 0) {
+				newArray[index++] = i;
+			}
+		}
+		for (int i = 0; i < newArray.length; i++) {
+			array[i] = newArray[i];
+		}
+	}
+	
+	public static void main(String[] args) {
+		int[] array = { 12, 2, 1, 8, 3 };
+		reOrderArray(array);
+		for (int i : array) {
+			System.out.print(i + " ");
+		}
+	}
+}
