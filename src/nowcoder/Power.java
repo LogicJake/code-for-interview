8431219161b56fb1ed55d1a40f5f554dbf597700
scy <835410808@qq.com>
2018-08-07 15:27:21 +0800

    剑指offer 数值的整数次方

---
+++ src/nowcoder/Power.java
@@ -0,0 +1,24 @@
+package nowcoder;
+
+public class Power {
+	public static double Power(double base, int exponent) {
+		double res = 1;
+		if (exponent < 0) {
+			exponent = -exponent;
+			for (int i = 0; i < exponent; i++) {
+				res = res * base;
+			}
+			return 1 / res;
+		}
+		for (int i = 0; i < exponent; i++) {
+			res = res * base;
+		}
+		return res;
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(Power(2, 0));
+		
+	}
+	
+}
