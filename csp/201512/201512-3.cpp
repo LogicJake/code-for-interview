68f72dd1dd6b88cd3ac7ae561d3c4d5ebd1c8a87
NepoleonAsus <835410808@qq.com>
2018-09-06 21:40:23 +0800

    完美

--- csp/201512/201512-3.cpp
+++ csp/201512/201512-3.cpp
@@ -5,24 +5,13 @@ using namespace std;
 int m, n, q;
 char **pic;
 
-vector<int> transformation(int x, int y)
-{
-    vector<int> res;
-    int x1, y1;
-    y1 = x;
-    x1 = n - 1 - y;
-    res.push_back(x1);
-    res.push_back(y1);
-    return res;
-}
-
 void draw()
 {
-    for (int i = 0; i < n; i++)
-    {        
+    for (int i = n - 1; i >= 0; i--)
+    {
         for (int j = 0; j < m; j++)
         {
-            cout << pic[i][j] << " ";
+            cout << pic[i][j];
         }
         cout << endl;
     }
@@ -30,15 +19,44 @@ void draw()
 
 void fill(int x, int y, char c)
 {
-    vector<int> res = transformation(x, y);
-    if (x >= m || x < 0 || y >= n || y < 0 || pic[res[0]][res[1]] == c || pic[res[0]][res[1]] == '-' || pic[res[0]][res[1]] == '|' || pic[res[0]][res[1]] == '+')
+    if (x >= m || x < 0 || y >= n || y < 0 || pic[y][x] == c || pic[y][x] == '-' || pic[y][x] == '|' || pic[y][x] == '+')
         return;
-    pic[res[0]][res[1]] = c;
+    pic[y][x] = c;
 
     fill(x, y + 1, c);
     fill(x, y - 1, c);
     fill(x + 1, y, c);
     fill(x - 1, y, c);
+}
+
+void drawLine(int x1, int y1, int x2, int y2)
+{
+    if (y1 == y2)
+    {
+        int start = x1 > x2 ? x2 : x1;
+        int end = x1 > x2 ? x1 : x2;
+
+        for (int i = start; i <= end; i++)
+        {
+            if (pic[y1][i] == '|' || pic[y1][i] == '+')
+                pic[y1][i] = '+';
+            else
+                pic[y1][i] = '-';
+        }
+    }
+    else
+    {
+        int start = y1 > y2 ? y2 : y1;
+        int end = y1 > y2 ? y1 : y2;
+
+        for (int i = start; i <= end; i++)
+        {
+            if (pic[i][x1] == '-' || pic[i][x1] == '+')
+                pic[i][x1] = '+';
+            else
+                pic[i][x1] = '|';
+        }
+    }
 }
 
 int main()
@@ -70,73 +88,9 @@ int main()
         //画线操作
         else if (type == 0)
         {
-            char c;
             int x1, y1, x2, y2;
             cin >> x1 >> y1 >> x2 >> y2;
-            if (y1 == y2)
-            {
-                c = '-';
-                if (x1 < x2)
-                {
-                    for (int i = x1; i <= x2; i++)
-                    {
-                        vector<int> a = transformation(i, y1);
-
-                        if (pic[a[0]][a[1]] == '|')
-                        {
-                            pic[a[0]][a[1]] = '+';
-                        }
-                        else
-                            pic[a[0]][a[1]] = c;
-                    }
-                }
-                else
-                {
-                    for (int i = x2; i <= x1; i++)
-                    {
-                        vector<int> a = transformation(i, y1);
-
-                        if (pic[a[0]][a[1]] == '|')
-                        {
-                            pic[a[0]][a[1]] = '+';
-                        }
-                        else
-                            pic[a[0]][a[1]] = c;
-                    }
-                }
-            }
-            else
-            {
-                c = '|';
-                if (y1 < y2)
-                {
-                    for (int i = y1; i <= y2; i++)
-                    {
-                        vector<int> a = transformation(x1, i);
-
-                        if (pic[a[0]][a[1]] == '-')
-                        {
-                            pic[a[0]][a[1]] = '+';
-                        }
-                        else
-                            pic[a[0]][a[1]] = c;
-                    }
-                }
-                else
-                {
-                    for (int i = y2; i <= y1; i++)
-                    {
-                        vector<int> a = transformation(x1, i);
-
-                        if (pic[a[0]][a[1]] == '-')
-                        {
-                            pic[a[0]][a[1]] = '+';
-                        }
-                        else
-                            pic[a[0]][a[1]] = c;
-                    }
-                }
-            }
+            drawLine(x1, y1, x2, y2);
         }
     }
 
