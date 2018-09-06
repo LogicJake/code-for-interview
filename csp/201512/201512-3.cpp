5d75983264f6f099f023b0524bd60aabb559f3f0
NepoleonAsus <835410808@qq.com>
2018-09-06 20:25:42 +0800

    201512-3

--- csp/201512/201512-3.cpp
+++ csp/201512/201512-3.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int m, n, q;
 char **pic;
 
-int *transformation(int x, int y)
+vector<int> transformation(int x, int y)
 {
-    int res[2];
+    vector<int> res;
     int x1, y1;
     y1 = x;
     x1 = n - 1 - y;
-    res[0] = x1;
-    res[1] = y1;
+    res.push_back(x1);
+    res.push_back(y1);
     return res;
 }
 
 void draw()
 {
     for (int i = 0; i < n; i++)
-    {
+    {        
         for (int j = 0; j < m; j++)
         {
             cout << pic[i][j] << " ";
@@ -29,13 +30,10 @@ void draw()
 
 void fill(int x, int y, char c)
 {
-    int *res = transformation(x, y);
-    if (pic[res[0]][res[1]] == c || pic[res[0]][res[1]] == '-' || pic[res[0]][res[1]] == '|' || pic[res[0]][res[1]] == '+' || x >= m || x < 0 || y >= n || y < 0)
+    vector<int> res = transformation(x, y);
+    if (x >= m || x < 0 || y >= n || y < 0 || pic[res[0]][res[1]] == c || pic[res[0]][res[1]] == '-' || pic[res[0]][res[1]] == '|' || pic[res[0]][res[1]] == '+')
         return;
     pic[res[0]][res[1]] = c;
-    cout << res[0] << " " << res[1] << endl;
-    draw();
-    return;
 
     fill(x, y + 1, c);
     fill(x, y - 1, c);
@@ -46,7 +44,6 @@ void fill(int x, int y, char c)
 int main()
 {
     cin >> m >> n >> q;
-    char **pic;
     pic = new char *[n];
     for (int i = 0; i < n; i++)
     {
@@ -56,7 +53,6 @@ int main()
             pic[i][j] = '.';
         }
     }
-
     for (int i = 0; i < q; i++)
     {
         int type;
@@ -84,7 +80,7 @@ int main()
                 {
                     for (int i = x1; i <= x2; i++)
                     {
-                        int *a = transformation(i, y1);
+                        vector<int> a = transformation(i, y1);
 
                         if (pic[a[0]][a[1]] == '|')
                         {
@@ -98,7 +94,7 @@ int main()
                 {
                     for (int i = x2; i <= x1; i++)
                     {
-                        int *a = transformation(i, y1);
+                        vector<int> a = transformation(i, y1);
 
                         if (pic[a[0]][a[1]] == '|')
                         {
@@ -116,7 +112,7 @@ int main()
                 {
                     for (int i = y1; i <= y2; i++)
                     {
-                        int *a = transformation(x1, i);
+                        vector<int> a = transformation(x1, i);
 
                         if (pic[a[0]][a[1]] == '-')
                         {
@@ -130,7 +126,7 @@ int main()
                 {
                     for (int i = y2; i <= y1; i++)
                     {
-                        int *a = transformation(x1, i);
+                        vector<int> a = transformation(x1, i);
 
                         if (pic[a[0]][a[1]] == '-')
                         {
@@ -143,4 +139,6 @@ int main()
             }
         }
     }
+
+    draw();
 }
