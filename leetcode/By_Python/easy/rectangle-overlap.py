8ed4a631e4d53a62ed96096ef974afe0d62ee0bb
Chenyang Shi <chenyang9608@qq.com>
2019-01-10 16:46:44 +0800

    update

--- leetcode/By_Python/easy/rectangle-overlap.py
+++ leetcode/By_Python/easy/rectangle-overlap.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 # @Author: LogicJake
 # @Date:   2019-01-10 12:59:29
-# @Last Modified time: 2019-01-10 13:08:05
+# @Last Modified time: 2019-01-10 16:46:33
 
 
 class Solution:
@@ -12,20 +12,10 @@ class Solution:
         :type rec2: List[int]
         :rtype: bool
         """
-        botton_left = (rec2[0], rec2[1])
-        top_right = (rec2[2], rec2[3])
-        botton_right = (rec2[2], rec2[1])
-        top_left = (rec2[0], rec2[3])
-
-        if botton_left[0] > rec1[0] and botton_left[0] < rec1[2] and botton_left[1] > rec1[1] and botton_left[1] < rec1[3]:
-            return True
-        if top_right[0] > rec1[0] and top_right[0] < rec1[2] and top_right[1] > rec1[1] and top_right[1] < rec1[3]:
-            return True
-        if botton_right[0] > rec1[0] and botton_right[0] < rec1[2] and botton_right[1] > rec1[1] and botton_right[1] < rec1[3]:
-            return True
-        if top_left[0] > rec1[0] and top_left[0] < rec1[2] and top_left[1] > rec1[1] and top_left[1] < rec1[3]:
-            return True
-        return False
+        return not (rec1[2] <= rec2[0] or  # left
+                    rec1[3] <= rec2[1] or  # bottom
+                    rec1[0] >= rec2[2] or  # right
+                    rec1[1] >= rec2[3])    # top
 
 if __name__ == '__main__':
     rec1 = [0, 0, 2, 2]
