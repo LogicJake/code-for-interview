5ff66691fe97a8ab5adc5f1ae5d0ab4ad913e831
scy_laptop <835410808@qq.com>
2020-08-15 16:58:36 +0800

    update

--- leetcode-cn/216.组合总和-iii.py
+++ leetcode-cn/216.组合总和-iii.py
@@ -7,10 +7,62 @@
 # @lc code=start
 from typing import List
 
+# class Solution:
+#     def helper(self, candidates, target):
+#         result = []
+
+#         last_num = -1
+#         for i, num in enumerate(candidates):
+#             if num == last_num:
+#                 continue
+
+#             last_num = num
+
+#             if num < target and i < len(candidates) - 1:
+#                 tmp = self.helper(candidates[i + 1:], target - num)
+
+#                 if len(tmp) != 0:
+#                     for t in tmp:
+#                         result.append([num] + t)
+
+#             elif num == target:
+#                 result.append([num])
+
+#         return result
+
+#     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+#         candidates = list(range(1, 10))
+#         results = self.helper(candidates, n)
+#         new_results = []
+
+#         for r in results:
+#             if len(r) == k:
+#                 new_results.append(r)
+
+#         return new_results
+
 
 class Solution:
-    def helper(self, candidates, target):
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        path = []
         result = []
+
+        candidates = list(range(1, 10))
+        self.dfs(candidates, n, path, result, k)
+
+        new_results = []
+        for r in result:
+            if len(r) == k:
+                new_results.append(r)
+
+        return result
+
+    def dfs(self, candidates, target, path, result, cur):
+        if cur < 0:
+            return
+
+        if target == 0 and cur == 0:
+            result.append(path[:])
 
         last_num = -1
         for i, num in enumerate(candidates):
@@ -18,29 +70,14 @@ class Solution:
                 continue
 
             last_num = num
+            new_target = target - num
 
-            if num < target and i < len(candidates) - 1:
-                tmp = self.helper(candidates[i + 1:], target - num)
+            if new_target < 0:
+                break
 
-                if len(tmp) != 0:
-                    for t in tmp:
-                        result.append([num] + t)
-
-            elif num == target:
-                result.append([num])
-
-        return result
-
-    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        candidates = list(range(1, 10))
-        results = self.helper(candidates, n)
-        new_results = []
-
-        for r in results:
-            if len(r) == k:
-                new_results.append(r)
-
-        return new_results
+            path.append(num)
+            self.dfs(candidates[i + 1:], new_target, path, result, cur - 1)
+            path.pop()
 
 
 # @lc code=end
--- leetcode-cn/40.组合总和-ii.py
+++ leetcode-cn/40.组合总和-ii.py
@@ -7,31 +7,60 @@
 # @lc code=start
 from typing import List
 
+# class Solution:
+#     def combinationSum2(self, candidates: List[int],
+#                         target: int) -> List[List[int]]:
+#         result = []
+#         candidates.sort()
+#         last_num = -1
+#         for i, num in enumerate(candidates):
+#             if num == last_num:
+#                 continue
+
+#             last_num = num
+
+#             if num < target and i < len(candidates) - 1:
+#                 tmp = self.combinationSum2(candidates[i + 1:], target - num)
+
+#                 if len(tmp) != 0:
+#                     for t in tmp:
+
+#                         result.append([num] + t)
+
+#             elif num == target:
+#                 result.append([num])
+
+#         return result
+
 
 class Solution:
     def combinationSum2(self, candidates: List[int],
                         target: int) -> List[List[int]]:
+        path = []
         result = []
+
         candidates.sort()
+        self.dfs(candidates, target, path, result)
+        return result
+
+    def dfs(self, candidates, target, path, result):
+        if target == 0:
+            result.append(path[:])
+
         last_num = -1
         for i, num in enumerate(candidates):
             if num == last_num:
                 continue
 
             last_num = num
+            new_target = target - num
 
-            if num < target and i < len(candidates) - 1:
-                tmp = self.combinationSum2(candidates[i + 1:], target - num)
+            if new_target < 0:
+                break
 
-                if len(tmp) != 0:
-                    for t in tmp:
-
-                        result.append([num] + t)
-
-            elif num == target:
-                result.append([num])
-
-        return result
+            path.append(num)
+            self.dfs(candidates[i+1:], new_target, path, result)
+            path.pop()
 
 
 # @lc code=end
