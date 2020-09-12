b1dc739305c799d05d391b654c4878023c8d97b5
scy_lab <835410808@qq.com>
2020-09-12 20:04:00 +0800

    update

---
+++ written-exam/趋势科技-删除最小.py
@@ -0,0 +1,30 @@
+def function(N, x):
+    m = len(N)
+
+    reset = {}
+    for i in range(len(N)):
+        reset[i] = m - i - 1
+
+    def help(start, reset_num):
+        min_c = '9'
+        min_index = 0
+        for i in range(start, len(N)):
+            if N[i] < min_c and reset[i] >= reset_num:
+                min_c = N[i]
+                min_index = i
+        return min_c, min_index
+
+    result = []
+    start = -1
+    for i in range(m - x):
+        start = start + 1
+        a, start = help(start, m - x - i - 1)
+        result.append(a)
+
+    while result[0] == '0':
+        result.pop(0)
+
+    return ''.join(result)
+
+
+print(function('2020', 1))
