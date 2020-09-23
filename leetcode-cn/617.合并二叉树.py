bfc96b7cad14eabdc5dcb369473d40aa4b014897
scy_lab <835410808@qq.com>
2020-09-23 20:23:48 +0800

    update

---
+++ leetcode-cn/617.合并二叉树.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=617 lang=python3
+#
+# [617] 合并二叉树
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def mergeTrees(self, t1: TreeNode, t2: TreeNode) -> TreeNode:
+        if not t1:
+            return t2
+        if not t2:
+            return t1
+
+        merged = TreeNode(t1.val + t2.val)
+        merged.left = self.mergeTrees(t1.left, t2.left)
+        merged.right = self.mergeTrees(t1.right, t2.right)
+        return merged
+
+
+# @lc code=end
