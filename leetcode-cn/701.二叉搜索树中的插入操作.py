1a3a340c29b40a0fbfbcecc84609269aa2864cc9
scy_lab <835410808@qq.com>
2020-09-30 09:43:48 +0800

    update

---
+++ leetcode-cn/701.二叉搜索树中的插入操作.py
@@ -0,0 +1,30 @@
+#
+# @lc app=leetcode.cn id=701 lang=python3
+#
+# [701] 二叉搜索树中的插入操作
+#
+
+
+# @lc code=start
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(self, root: TreeNode, val: int) -> TreeNode:
+        if root is None:
+            return TreeNode(val)
+
+        if val > root.val:
+            root.right = self.insertIntoBST(root.right, val)
+        elif val < root.val:
+            root.left = self.insertIntoBST(root.left, val)
+
+        return root
+
+
+# @lc code=end
