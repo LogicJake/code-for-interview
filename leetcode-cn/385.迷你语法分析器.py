37717fc60b73b25e233b144f9160c92f4d8af464
scy_laptop <835410808@qq.com>
2020-10-03 10:36:35 +0800

    update

--- leetcode-cn/385.迷你语法分析器.py
+++ leetcode-cn/385.迷你语法分析器.py
@@ -9,45 +9,75 @@
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger:
-#    def __init__(self, value=None):
-#        """
+# class NestedInteger:
+#     def __init__(self, value=None):
+#         """
 #        If value is not specified, initializes an empty list.
 #        Otherwise initializes a single integer equal to value.
 #        """
-#
-#    def isInteger(self):
-#        """
+#     def isInteger(self):
+#         """
 #        @return True if this NestedInteger holds a single integer, rather than a nested list.
 #        :rtype bool
 #        """
-#
-#    def add(self, elem):
-#        """
+#     def add(self, elem):
+#         """
 #        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
 #        :rtype void
 #        """
-#
-#    def setInteger(self, value):
-#        """
+#     def setInteger(self, value):
+#         """
 #        Set this NestedInteger to hold a single integer equal to value.
 #        :rtype void
 #        """
-#
-#    def getInteger(self):
-#        """
+#     def getInteger(self):
+#         """
 #        @return the single integer that this NestedInteger holds, if it holds a single integer
 #        Return None if this NestedInteger holds a nested list
 #        :rtype int
 #        """
-#
-#    def getList(self):
-#        """
+#     def getList(self):
+#         """
 #        @return the nested list that this NestedInteger holds, if it holds a nested list
 #        Return None if this NestedInteger holds a single integer
 #        :rtype List[NestedInteger]
 #        """
+
+
 class Solution:
     def deserialize(self, s: str) -> NestedInteger:
-# @lc code=end
+        if s[0] != '[':
+            return NestedInteger(int(s))
 
+        num = 0
+        sign = 1
+        is_num = False
+        stack = []
+
+        for c in s:
+            if c.isdigit():
+                num = num * 10 + int(c)
+                is_num = True
+
+            elif c == '-':
+                sign = -1
+            elif c == '[':
+                stack.append(NestedInteger())
+            elif c == ']' or c == ',':
+                if is_num:
+                    last_nes = stack.pop()
+                    last_nes.add(NestedInteger(num * sign))
+                    stack.append(last_nes)
+
+                num = 0
+                sign = 1
+                is_num = False
+
+                if c == ']' and len(stack) > 1:
+                    last_nes = stack.pop()
+                    stack[-1].add(last_nes)
+
+        return stack[0]
+
+
+# @lc code=end
