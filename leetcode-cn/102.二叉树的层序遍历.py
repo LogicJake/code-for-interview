4b8949966eb84b10ddc1f10ee05ad0b749586d6a
scy_lab <835410808@qq.com>
2020-10-12 21:25:08 +0800

    update

---
+++ leetcode-cn/102.二叉树的层序遍历.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=102 lang=python3
+#
+# [102] 二叉树的层序遍历
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if root is None:
+            return []
+
+        queue = [root]
+        ans = []
+        while queue:
+            sz = len(queue)
+            print(sz)
+
+            nums = []
+            for _ in range(sz):
+                node = queue.pop(0)
+                nums.append(node.val)
+
+                if node.left is not None:
+                    queue.append(node.left)
+                if node.right is not None:
+                    queue.append(node.right)
+
+            ans.append(nums)
+
+        return ans
+
+
+# @lc code=end
