610d42d1d030052ac00cbbffbacd33a7bb6780ff
scy_lab <835410808@qq.com>
2020-10-14 21:14:22 +0800

    update

--- leetcode-cn/889.根据前序和后序遍历构造二叉树.py
+++ leetcode-cn/889.根据前序和后序遍历构造二叉树.py
@@ -6,14 +6,31 @@
 
 # @lc code=start
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
-    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
-        
-# @lc code=end
+    def constructFromPrePost(self, pre: List[int],
+                             post: List[int]) -> TreeNode:
+        if len(pre) == 0:
+            return None
 
+        root = TreeNode(pre[0])
+        if len(pre) == 1:
+            return root
+
+        L = post.index(pre[1]) + 1
+        root.left = self.constructFromPrePost(pre[1:L + 1], post[:L])
+        root.right = self.constructFromPrePost(pre[L + 1:], post[L:-1])
+
+        return root
+
+
+# @lc code=end
