f28d0d2922efaa5388f31b5dedf67e5132e99757
scy_laptop <835410808@qq.com>
2020-10-22 23:04:28 +0800

    update

--- leetcode-cn/198.打家劫舍.py
+++ leetcode-cn/198.打家劫舍.py
@@ -13,15 +13,11 @@ class Solution:
         n = len(nums)
         if n == 0:
             return 0
-        if n == 1:
-            return nums[0]
-
         dp = [0] * n
         dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
 
-        for i in range(2, n):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        for i in range(1, n):
+            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
 
         return dp[-1]
 
---
+++ leetcode-cn/70.爬楼梯.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=70 lang=python3
+#
+# [70] 爬楼梯
+#
+
+
+# @lc code=start
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[-1]
+
+
+# @lc code=end
