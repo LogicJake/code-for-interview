a8fc7e74a6ff627cea23aad232027358a4cc537f
scy_lab <835410808@qq.com>
2020-10-24 09:39:04 +0800

    update

--- leetcode-cn/32.最长有效括号.py
+++ leetcode-cn/32.最长有效括号.py
@@ -8,23 +8,26 @@
 # @lc code=start
 class Solution:
     def longestValidParentheses(self, s: str) -> int:
-        dp = [0] * len(s)
+        dp = [0] * (len(s))
         ans = 0
-        for i in range(1, len(s)):
-            if s[i] == ')':
-                if s[i - 1] == '(':
-                    if i - 2 >= 0:
-                        dp[i] = dp[i - 2] + 2
-                    else:
-                        dp[i] = 2
-                else:
-                    if i - dp[i - 1] - 1 >= 0 and s[i - dp[i - 1] - 1] == '(':
-                        if i - dp[i - 1] - 2 >= 0:
-                            dp[i] = dp[i - dp[i - 1] - 2] + dp[i - 1] + 2
-                        else:
-                            dp[i] = dp[i - 1] + 2
 
-                ans = max(ans, dp[i])
+        for i, c in enumerate(s):
+            if c == ')':
+                if i - 1 >= 0:
+                    if s[i - 1] == '(':
+                        if i - 2 >= 0:
+                            dp[i] = dp[i - 2] + 2
+                        else:
+                            dp[i] = 2
+                    elif s[i - 1] == ')':
+                        if i - 1 - dp[i - 1] >= 0 and s[i - 1 -
+                                                        dp[i - 1]] == '(':
+                            if i - 2 - dp[i - 1] >= 0:
+                                dp[i] = dp[i - 1] + dp[i - 2 - dp[i - 1]] + 2
+                            else:
+                                dp[i] = dp[i - 1] + 2
+                    ans = max(ans, dp[i])
+
         return ans
 
 
--- leetcode-cn/53.最大子序和.py
+++ leetcode-cn/53.最大子序和.py
@@ -11,14 +11,11 @@ from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return nums[0]
-
         dp = [0] * n
         dp[0] = nums[0]
 
         for i in range(1, n):
-            dp[i] = max(nums[i], dp[i - 1] + nums[i])
+            dp[i] = max(dp[i - 1] + nums[i], nums[i])
 
         return max(dp)
 
