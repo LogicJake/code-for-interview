1548dbec7cbebb6d8352d4eaa3da15b505d84ba7
scy_lab <835410808@qq.com>
2020-10-26 16:54:47 +0800

    update

---
+++ leetcode-cn/87.扰乱字符串.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=87 lang=python3
+#
+# [87] 扰乱字符串
+#
+
+
+# @lc code=start
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+        if len(s1) != len(s2):
+            return False
+        if s1 == s2:
+            return True
+        if sorted(s1) != sorted(s2):
+            return False
+
+        for i in range(1, len(s1)):
+            if self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:]) or \
+                    (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])):
+                return True
+        return False
+
+
+# @lc code=end
