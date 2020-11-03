aa2013092430d1148706bf616e49dd68de0d0e3e
scy_lab <835410808@qq.com>
2020-11-03 15:21:30 +0800

    update

--- leetcode-cn/1.两数之和.py
+++ leetcode-cn/1.两数之和.py
@@ -11,32 +11,14 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i, value1 in enumerate(nums):
-            # 这里每一次迭代都要重置，否则 result 会保存上一轮的无用结果
-            result = []
+        mem = {}
 
-            # 可以出现 = ，即有一个整数为0
-            '''
-            if value1 <= target:
-                result.append(i)
-                target2 = target - value1
+        for i, num in enumerate(nums):
+            if target - num in mem:
+                return [mem[target - num], i]
 
-                for j, value2 in enumerate(nums):
-                    if j == i:
-                        continue
-                    if value2 == target2:
-                        result.append(j)
-                        return result
-            '''
-            result.append(i)
-            target2 = target - value1
-
-            for j, value2 in enumerate(nums):
-                if j == i:
-                    continue
-                if value2 == target2:
-                    result.append(j)
-                    return result
+            else:
+                mem[num] = i
 
 
 # @lc code=end
--- leetcode-cn/15.三数之和.py
+++ leetcode-cn/15.三数之和.py
@@ -10,28 +10,30 @@ from typing import List
 
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        ans = []
         nums.sort()
+        n = len(nums)
 
-        for first in range(len(nums)):
+        for first in range(n):
             if first > 0 and nums[first] == nums[first - 1]:
                 continue
 
+            third = n - 1
             target = 0 - nums[first]
-            third = len(nums) - 1
-            for second in range(first + 1, len(nums)):
+            for second in range(first + 1, n):
                 if second > first + 1 and nums[second] == nums[second - 1]:
                     continue
 
-                while second < third and nums[second] + nums[third] > target:
+                while third > second and nums[second] + nums[third] > target:
                     third -= 1
 
-                if second == third:
+                if third == second:
                     break
 
                 if nums[second] + nums[third] == target:
-                    res.append([nums[first], nums[second], nums[third]])
-        return res
+                    ans.append([nums[first], nums[second], nums[third]])
+
+        return ans
 
 
 # @lc code=end
