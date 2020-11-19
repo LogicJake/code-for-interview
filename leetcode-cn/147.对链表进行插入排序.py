c05fab5c50b649ccd38f0940681842720f2054c1
scy_lab <835410808@qq.com>
2020-11-19 14:32:52 +0800

    update

---
+++ leetcode-cn/147.对链表进行插入排序.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=147 lang=python3
+#
+# [147] 对链表进行插入排序
+#
+
+
+# @lc code=start
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        pass
+
+
+# @lc code=end
