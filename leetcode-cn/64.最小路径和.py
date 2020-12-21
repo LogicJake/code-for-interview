efb615cb2ff4687377c0f56b043e68b02f180dc8
scy_lab <835410808@qq.com>
2020-12-21 09:33:48 +0800

    update

--- leetcode-cn/64.最小路径和.py
+++ leetcode-cn/64.最小路径和.py
@@ -15,15 +15,16 @@ class Solution:
 
         dp = [[float('inf')] * n for _ in range(m)]
         dp[0][0] = grid[0][0]
-        for j in range(1, n):
-            dp[0][j] = dp[0][j - 1] + grid[0][j]
+
+        for i in range(1, n):
+            dp[0][i] = dp[0][i - 1] + grid[0][i]
 
         for i in range(1, m):
             dp[i][0] = dp[i - 1][0] + grid[i][0]
 
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
+                dp[i][j] = min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j]
 
         return dp[-1][-1]
 
