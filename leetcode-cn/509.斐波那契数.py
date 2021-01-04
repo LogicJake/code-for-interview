008e75dc8924b706f0f9f5bc7ead5efdf3145917
scy_lab <835410808@qq.com>
2021-01-04 22:11:15 +0800

    update

--- leetcode-cn/509.斐波那契数.py
+++ leetcode-cn/509.斐波那契数.py
@@ -12,12 +12,13 @@ class Solution:
             return N
 
         dp = [0] * (N + 1)
-        # 进行状态转移
+        dp[0] = 0
         dp[1] = 1
+
         for i in range(2, N + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
 
-        return dp[N]
+        return dp[-1]
 
 
 # @lc code=end
