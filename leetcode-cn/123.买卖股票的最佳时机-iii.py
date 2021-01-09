89708d18641c9a960d551b3a0f934722fdd396ec
scy_laptop <835410808@qq.com>
2021-01-09 09:03:23 +0800

    update

--- leetcode-cn/123.买卖股票的最佳时机-iii.py
+++ leetcode-cn/123.买卖股票的最佳时机-iii.py
@@ -11,26 +11,15 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         n = len(prices)
-        max_k = 2
-        dp = [[[0, 0] for _ in range(max_k + 1)] for _ in range(n)]
 
-        for i in range(n):
-            dp[i][0][0] = 0
-            dp[i][0][1] = -float('inf')
-
-        for i in range(n):
-            for k in range(max_k, 0, -1):
-                if i == 0:
-                    dp[i][k][0] = 0
-                    dp[i][k][1] = -prices[0]
-                else:
-                    dp[i][k][0] = max(dp[i - 1][k][0],
-                                      dp[i - 1][k][1] + prices[i])
-
-                    dp[i][k][1] = max(dp[i - 1][k][1],
-                                      dp[i - 1][k - 1][0] - prices[i])
-
-        return dp[-1][max_k][0]
+        buy1 = buy2 = -prices[0]
+        sell1 = sell2 = 0
+        for i in range(1, n):
+            buy1 = max(buy1, -prices[i])
+            sell1 = max(sell1, buy1 + prices[i])
+            buy2 = max(buy2, sell1 - prices[i])
+            sell2 = max(sell2, buy2 + prices[i])
+        return sell2
 
 
 # @lc code=end
