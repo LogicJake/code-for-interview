e37cea10ed382d2373b5d256c79507a625187e75
scy_lab <835410808@qq.com>
2021-01-13 10:16:30 +0800

    update

---
+++ leetcode-cn/684.冗余连接.py
@@ -0,0 +1,33 @@
+#
+# @lc app=leetcode.cn id=684 lang=python3
+#
+# [684] 冗余连接
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        nodesCount = len(edges)
+        parent = list(range(nodesCount + 1))
+
+        def find(index: int) -> int:
+            if parent[index] != index:
+                parent[index] = find(parent[index])
+            return parent[index]
+
+        def union(index1: int, index2: int):
+            parent[find(index1)] = find(index2)
+
+        for node1, node2 in edges:
+            if find(node1) != find(node2):
+                union(node1, node2)
+            else:
+                return [node1, node2]
+
+        return []
+
+
+# @lc code=end
