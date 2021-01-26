d77e32459ee1f7ca680be0fdf5f052594b510852
scy_lab <835410808@qq.com>
2021-01-26 20:55:27 +0800

    update

---
+++ leetcode-cn/1128.等价多米诺骨牌对的数量.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode.cn id=1128 lang=python3
+#
+# [1128] 等价多米诺骨牌对的数量
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def numEquivDominoPairs(self, dominoes: List[List[int]]) -> int:
+        num = [0] * 100
+        ret = 0
+        for x, y in dominoes:
+            val = (x * 10 + y if x <= y else y * 10 + x)
+            ret += num[val]
+            num[val] += 1
+        return ret
+
+
+# @lc code=end
