66003365b9d058d9260ae25907bc939112fdb108
scy_laptop <835410808@qq.com>
2021-01-29 17:59:56 +0800

    update

--- 剑指offer/19.正则表达式匹配.py
+++ 剑指offer/19.正则表达式匹配.py
@@ -1,3 +1,24 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pass
+        m, n = len(s) + 1, len(p) + 1
+        dp = [[False] * n for _ in range(m)]
+        dp[0][0] = True
+        # 初始化首行
+        for j in range(2, n, 2):
+            dp[0][j] = dp[0][j - 2] and p[j - 1] == '*'
+        # 状态转移
+        for i in range(1, m):
+            for j in range(1, n):
+                if p[j - 1] == '*':
+                    if dp[i][j - 2]: dp[i][j] = True  # 1.
+                    elif dp[i][j - 1]: dp[i][j] = True  # 2.
+                    elif dp[i - 1][j] and s[i - 1] == p[j - 2]:
+                        dp[i][j] = True  # 3.
+                    elif dp[i - 1][j] and p[j - 2] == '.':
+                        dp[i][j] = True  # 4.
+                else:
+                    if dp[i - 1][j - 1] and s[i - 1] == p[j - 1]:
+                        dp[i][j] = True  # 1.
+                    elif dp[i - 1][j - 1] and p[j - 1] == '.':
+                        dp[i][j] = True  # 2.
+        return dp[-1][-1]
