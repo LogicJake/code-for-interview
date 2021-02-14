b885a90151c032c58aff4c4d8c737597ea417516
scy_laptop <835410808@qq.com>
2021-02-14 15:46:47 +0800

    update

---
+++ leetcode-cn/765.情侣牵手.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=765 lang=python3
+#
+# [765] 情侣牵手
+#
+
+
+# @lc code=start
+class Solution(object):
+    def minSwapsCouples(self, row):
+        """
+        :type row: List[int]
+        :rtype: int
+        """
+        N = len(row)
+        res = 0
+        for i in range(0, N - 1, 2):
+            if row[i] == row[i + 1] ^ 1:
+                continue
+            for j in range(i + 1, N):
+                if row[i] == row[j] ^ 1:
+                    row[i + 1], row[j] = row[j], row[i + 1]
+            res += 1
+        return res
+
+
+# @lc code=end
