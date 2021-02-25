f2c76c2e0c1921c701bee26fa10649fd54d7b729
scy_laptop <835410808@qq.com>
2021-02-25 10:30:10 +0800

    update

---
+++ leetcode-cn/867.转置矩阵.py
@@ -0,0 +1,25 @@
+#
+# @lc app=leetcode.cn id=867 lang=python3
+#
+# [867] 转置矩阵
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
+        m = len(matrix)
+        n = len(matrix[0])
+
+        ans = [[] for _ in range(n)]
+
+        for i in range(m):
+            for j in range(n):
+                ans[j].append(matrix[i][j])
+
+        return ans
+
+
+# @lc code=end
