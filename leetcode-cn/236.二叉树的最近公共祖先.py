d3a673a27dd9147ca3b72d3737048d810727c08c
scy_laptop <835410808@qq.com>
2021-03-01 22:43:56 +0800

    update

--- leetcode-cn/236.二叉树的最近公共祖先.py
+++ leetcode-cn/236.二叉树的最近公共祖先.py
@@ -14,51 +14,21 @@ class TreeNode:
         self.right = None
 
 
-# class Solution:
-#     def dfs(self, root, fa):
-#         if root.left is not None:
-#             fa[root.left.val] = root
-#             self.dfs(root.left, fa)
-
-#         if root.right is not None:
-#             fa[root.right.val] = root
-#             self.dfs(root.right, fa)
-
-#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
-#                              q: 'TreeNode') -> 'TreeNode':
-#         fa = {}
-#         fa[root.val] = None
-#         visited = {}
-#         self.dfs(root, fa)
-
-#         while p is not None:
-#             visited[p.val] = True
-#             p = fa[p.val]
-
-#         while q is not None:
-#             if q.val in visited and visited[q.val]:
-#                 return q
-#             q = fa[q.val]
-
-
 class Solution:
-    def dfs(self, root, p, q):
-        if root is None:
-            return False
-        lson = self.dfs(root.left, p, q)
-        rson = self.dfs(root.right, p, q)
-
-        if lson and rson or ((root.val == p.val or root.val == q.val) and
-                             (lson or rson)):
-            self.ans = root
-
-        return lson or rson or root.val == p.val or root.val == q.val
-
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode',
                              q: 'TreeNode') -> 'TreeNode':
-        self.ans = None
-        self.dfs(root, p, q)
-        return self.ans
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+
+        if not left:
+            return right
+        if not right:
+            return left
+
+        return root
 
 
 # @lc code=end
