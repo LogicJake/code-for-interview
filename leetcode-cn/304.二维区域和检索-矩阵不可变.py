0964c055d41ea61a50f971d73c2294a5d783e2d9
scy_lab <835410808@qq.com>
2021-03-02 10:56:19 +0800

    update

--- leetcode-cn/304.二维区域和检索-矩阵不可变.py
+++ leetcode-cn/304.二维区域和检索-矩阵不可变.py
@@ -10,40 +10,32 @@ from typing import List
 
 class NumMatrix:
     def __init__(self, matrix: List[List[int]]):
-        # 行
         m = len(matrix)
-        # 列
+
         if m == 0:
             return
 
         n = len(matrix[0])
 
-        sums = [[0 for i in range(n)] for j in range(m)]
-        for i in range(n):
-            for j in range(m):
-                if j == 0:
-                    sums[j][i] = sum([matrix[0][a] for a in range(i + 1)])
-                else:
-                    sums[j][i] = sums[j - 1][i] + sum(
-                        [matrix[j][a] for a in range(0, i + 1)])
-        self.sums = sums
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[0][0] = matrix[0][0]
+
+        for i in range(1, n):
+            dp[0][i] = dp[0][i - 1] + matrix[0][i]
+
+        for i in range(1, m):
+            dp[i][0] = dp[i - 1][0] + matrix[i][0]
+
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][
+                    j - 1] + matrix[i][j]
+
+        self.dp = dp
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        left_up = [row1, col1]
-        right_up = [row1, col2]
-
-        left_down = [row2, col1]
-        right_down = [row2, col2]
-
-        part1 = self.sums[right_down[0]][right_down[1]]
-        part2 = 0 if right_up[0] == 0 else self.sums[right_up[0] -
-                                                     1][right_up[1]]
-        part3 = 0 if left_down[1] == 0 else self.sums[left_down[0]][
-            left_down[1] - 1]
-        part4 = 0 if left_up[1] == 0 or left_up[0] == 0 else self.sums[
-            left_up[0] - 1][left_up[1] - 1]
-
-        return part1 - part2 - part3 + part4
+        return self.dp[row2][col2] - self.dp[row1 - 1][col2] - self.dp[row2][
+            col1 - 1] + self.dp[row1 - 1][col1 - 1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
