a06e12b60d97a458f1bccca789775d4c4871e311
scy_lab <835410808@qq.com>
2021-03-03 18:45:05 +0800

    update

---
+++ leetcode-cn/1143.最长公共子序列.py
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode.cn id=1143 lang=python3
+#
+# [1143] 最长公共子序列
+#
+
+
+# @lc code=start
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+
+        return dp[-1][-1]
+
+
+# @lc code=end
