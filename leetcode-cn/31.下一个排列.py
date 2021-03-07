9d9f966fe64e4537a2fe81148fb89a71053762ad
scy_lab <835410808@qq.com>
2021-03-07 20:55:37 +0800

    update

--- leetcode-cn/31.下一个排列.py
+++ leetcode-cn/31.下一个排列.py
@@ -14,18 +14,19 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
 
-        i = len(nums) - 2
-        while i > -1 and nums[i] >= nums[i + 1]:
+        # 从后向前搜索找到第一个小于后一个数的值
+        n = len(nums)
+
+        i = n - 2
+        while i >= 0 and nums[i] >= nums[i + 1]:
             i -= 1
 
         if i != -1:
-            j = len(nums) - 1
-            while (nums[j] <= nums[i]) and j >= 0:
+            j = n - 1
+            while j >= 0 and nums[j] <= nums[i]:
                 j -= 1
 
-            tmp = nums[j]
-            nums[j] = nums[i]
-            nums[i] = tmp
+            nums[i], nums[j] = nums[j], nums[i]
 
         nums[i + 1:] = sorted(nums[i + 1:])
 
