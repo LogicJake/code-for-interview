833ebbcc324e667936f873a5dd58c014430f5744
scy_lab <835410808@qq.com>
2021-03-08 13:20:55 +0800

    update

--- leetcode-cn/106.从中序与后序遍历序列构造二叉树.py
+++ leetcode-cn/106.从中序与后序遍历序列构造二叉树.py
@@ -8,35 +8,26 @@
 # Definition for a binary tree node.
 from typing import List
 
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 
 class Solution:
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
-        val_index = {}
-        for i, val in enumerate(inorder):
-            val_index[val] = i
+        if not postorder:
+            return None
+        root_val = postorder[-1]
+        root = TreeNode(root_val)
 
-        def help(left, right):
-            if left > right:
-                return None
+        index = inorder.index(root_val)
 
-            root_val = postorder.pop()
-            root = TreeNode(root_val)
+        root.left = self.buildTree(inorder[:index], postorder[:index])
+        root.right = self.buildTree(inorder[index + 1:], postorder[index:-1])
 
-            index = val_index[root_val]
-
-            root.right = help(index + 1, right)
-            root.left = help(left, index - 1)
-
-            return root
-
-        return help(0, len(inorder) - 1)
+        return root
 
 
 # @lc code=end
