912d004ebbf09740089718071ce3f09fcc26e9a1
scy_lab <835410808@qq.com>
2021-03-23 10:46:39 +0800

    update

---
+++ nowcoder/3.链表中环的入口节点.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x)
+        : val(x)
+        , next(NULL)
+    {
+    }
+};
+
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head)
+    {
+        if (head == NULL) {
+            return NULL;
+        }
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (slow != NULL && fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) {
+                    slow = slow->next;
+                    fast = fast->next;
+                }
+                return slow;
+            }
+        }
+        return NULL;
+    }
+};
