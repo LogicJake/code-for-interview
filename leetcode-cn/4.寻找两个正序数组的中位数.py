8c2e3d565e9aa70fe75aa4c8781bf19f60a8f222
scy_lab <835410808@qq.com>
2021-03-24 20:49:45 +0800

    update

--- leetcode-cn/4.寻找两个正序数组的中位数.py
+++ leetcode-cn/4.寻找两个正序数组的中位数.py
@@ -11,57 +11,39 @@ from typing import List
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int],
                                nums2: List[int]) -> float:
+        def help(nums1, start1, end1, nums2, start2, end2, k):
+            len1 = end1 - start1 + 1
+            len2 = end2 - start2 + 1
+
+            # 保证 nums1 较短
+            if len1 > len2:
+                return help(nums2, start2, end2, nums1, start1, end1, k)
+
+            if len1 == 0:
+                return nums2[start2 + k - 1]
+
+            if k == 1:
+                return min(nums1[start1], nums2[start2])
+
+            i = start1 + min(k // 2, len1) - 1
+            j = start2 + min(k // 2, len2) - 1
+
+            if nums1[i] <= nums2[j]:
+                return help(nums1, i + 1, end1, nums2, start2, end2,
+                            k - (i - start1 + 1))
+            else:
+                return help(nums1, start1, end1, nums2, j + 1, end2,
+                            k - (j - start2 + 1))
+
         m = len(nums1)
         n = len(nums2)
 
-        i = 0
-        j = 0
-        global_index = 0
-        if (m + n) % 2 == 0:
-            index1 = (m + n) // 2
-            index2 = index1 - 1
-            result = 0
-            count = 0
-            while i < m or j < n:
-                if (i < m and j < n and nums1[i] < nums2[j]) or (i < m
-                                                                 and j >= n):
-                    if global_index == index1 or global_index == index2:
-                        result += nums1[i]
-                        count += 1
-
-                        if count == 2:
-                            return result / 2
-
-                    i += 1
-                    global_index += 1
-                else:
-                    if global_index == index1 or global_index == index2:
-                        result += nums2[j]
-                        count += 1
-
-                        if count == 2:
-                            return result / 2
-
-                    j += 1
-                    global_index += 1
-
+        if (m + n) % 2 == 1:
+            return help(nums1, 0, m - 1, nums2, 0, n - 1, (m + n + 1) // 2)
         else:
-            index = (m + n) // 2
-
-            while i < m or j < n:
-                if (i < m and j < n and nums1[i] < nums2[j]) or (i < m
-                                                                 and j >= n):
-                    if global_index == index:
-                        return nums1[i]
-
-                    i += 1
-                    global_index += 1
-                else:
-                    if global_index == index:
-                        return nums2[j]
-
-                    j += 1
-                    global_index += 1
+            return (help(nums1, 0, m - 1, nums2, 0, n - 1,
+                         (m + n) // 2) + help(nums1, 0, m - 1, nums2, 0, n - 1,
+                                              (m + n) // 2 + 1)) / 2
 
 
 # @lc code=end
