d12411cb0ea37b6bcd21dadde44381014d9ba0ea
scy_lab <835410808@qq.com>
2021-04-02 16:29:37 +0800

    update

---
+++ 程序员面试宝典/17.21.直方图的水量.py
@@ -0,0 +1,17 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        ans = 0
+        stack = list()
+
+        for i, h in enumerate(height):
+            while stack and h > height[stack[-1]]:
+                top = stack.pop()
+                if not stack:
+                    break
+                left = stack[-1]
+                currWidth = i - left - 1
+                currHeight = min(height[left], height[i]) - height[top]
+                ans += currWidth * currHeight
+            stack.append(i)
+
+        return ans
