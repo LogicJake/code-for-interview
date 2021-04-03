42e56a18cee7d4a9b89bac4ab85c9da33f78a735
scy_laptop <835410808@qq.com>
2021-04-03 17:07:15 +0800

    update

--- leetcode-cn/1143.最长公共子序列.py
+++ leetcode-cn/1143.最长公共子序列.py
@@ -18,7 +18,7 @@ class Solution:
                 if text1[i - 1] == text2[j - 1]:
                     dp[i][j] = dp[i - 1][j - 1] + 1
                 else:
-                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
         return dp[-1][-1]
 
