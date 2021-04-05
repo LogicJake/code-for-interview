c6825e9f0d5b00ba7d2485bec280e3a7e8a1c435
scy_lab <835410808@qq.com>
2021-04-05 16:46:30 +0800

    update

--- leetcode-cn/88.合并两个有序数组.py
+++ leetcode-cn/88.合并两个有序数组.py
@@ -14,28 +14,31 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m - 1
-        j = n - 1
-        p = m + n - 1
+        totol_len = len(nums1)
 
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
-                nums1[p] = nums1[i]
-                i -= 1
+        i = totol_len - 1
+        p = m - 1
+        q = n - 1
+
+        while p >= 0 and q >= 0:
+            if nums1[p] >= nums2[q]:
+                nums1[i] = nums1[p]
                 p -= 1
             else:
-                nums1[p] = nums2[j]
-                j -= 1
-                p -= 1
+                nums1[i] = nums2[q]
+                q -= 1
 
-        while i >= 0:
-            nums1[p] = nums1[i]
             i -= 1
+
+        while q >= 0:
+            nums1[i] = nums2[q]
+            q -= 1
+            i -= 1
+
+        while p >= 0:
+            nums1[i] = nums1[p]
             p -= 1
-        while j >= 0:
-            nums1[p] = nums2[j]
-            j -= 1
-            p -= 1
+            i -= 1
 
 
 # @lc code=end
