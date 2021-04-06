9d3f47d47ebe6f5b2291fd0ee9cabceaf1c6ba13
scy_lab <835410808@qq.com>
2021-04-06 23:10:18 +0800

    update

---
+++ leetcode-cn/80.删除有序数组中的重复项-ii.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode.cn id=80 lang=python3
+#
+# [80] 删除有序数组中的重复项 II
+#
+
+# @lc code=start
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        slow = 0
+        for fast in range(len(nums)):
+            if slow < 2 or nums[fast] != nums[slow - 2]:
+                nums[slow] = nums[fast]
+                slow += 1
+        return slow
+
+
+# @lc code=end
