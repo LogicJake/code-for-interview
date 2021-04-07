7ed88dbe7fcf66e41451acde609ed4f682aea877
scy_lab <835410808@qq.com>
2021-04-07 17:11:35 +0800

    update

--- leetcode-cn/33.搜索旋转排序数组.py
+++ leetcode-cn/33.搜索旋转排序数组.py
@@ -9,37 +9,27 @@ from typing import List
 
 
 class Solution:
-    def binarySearch(self, nums, target):
-        i = 0
-        j = len(nums) - 1
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
 
-        while i <= j:
-            mid = (i + j) // 2
+        while left <= right:
+            mid = (left + right) // 2
 
-            if nums[mid] < target:
-                i += 1
-            elif nums[mid] > target:
-                j -= 1
-            else:
+            if nums[mid] == target:
                 return mid
+            if nums[mid] >= nums[left]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
 
         return -1
-
-    def search(self, nums: List[int], target: int) -> int:
-        i = 0
-        while i < len(nums) - 1 and nums[i] < nums[i + 1]:
-            i += 1
-
-        left_res = self.binarySearch(nums[:i + 1], target)
-
-        if left_res != -1:
-            return left_res
-
-        right_res = self.binarySearch(nums[i + 1:], target)
-        if right_res != -1:
-            return i + 1 + right_res
-        else:
-            return -1
 
 
 # @lc code=end
