10154196306d8f2dde16281a7485cee9db09b7af
scy_laptop <835410808@qq.com>
2021-04-16 00:36:19 +0800

    update

---
+++ leetcode-cn/C++/7.整数反转.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=7 lang=cpp
+ *
+ * [7] 整数反转
+ */
+
+// @lc code=start
+#include <limits.h>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x)
+    {
+        long int ans = 0;
+        while (x) {
+            int pop = x % 10;
+            x = x / 10;
+
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && pop > 7))
+                return 0;
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && pop < -8))
+                return 0;
+
+            ans = ans * 10 + pop;
+        }
+
+        return ans;
+    }
+};
+// @lc code=end
