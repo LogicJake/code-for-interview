97816bc0f09d72d1d04cfe58f67ba59dab32d03b
scy_laptop <835410808@qq.com>
2021-04-17 00:00:31 +0800

    update

---
+++ leetcode-cn/C++/11.盛最多水的容器.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=cpp
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxArea(vector<int>& height)
+    {
+        int left = 0;
+        int right = height.size() - 1;
+
+        int ans = 0;
+        while (left < right) {
+            ans = max(ans, min(height[left], height[right]) * (right - left));
+            if (height[left] > height[right]) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return ans;
+    }
+};
+// @lc code=end
