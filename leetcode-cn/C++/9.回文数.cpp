417995dae3ebbb8e927720a3bd64cbd788afadb1
scy_laptop <835410808@qq.com>
2021-04-16 23:51:43 +0800

    update

---
+++ leetcode-cn/C++/9.回文数.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=cpp
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+
+        int reverseNum = 0;
+        while (x > reverseNum) {
+            reverseNum = reverseNum * 10 + x % 10;
+            x /= 10;
+        }
+
+        return x == reverseNum / 10 || x == reverseNum;
+    }
+};
+// @lc code=end
