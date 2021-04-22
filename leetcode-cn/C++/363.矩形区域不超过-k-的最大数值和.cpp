45013ecff6ddf40e6aae7ed66a6091c31c343bef
scy_lab <835410808@qq.com>
2021-04-22 14:49:59 +0800

    update

---
+++ leetcode-cn/C++/363.矩形区域不超过-k-的最大数值和.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=363 lang=cpp
+ *
+ * [363] 矩形区域不超过 K 的最大数值和
+ */
+
+// @lc code=start
+#include <limits.h>
+#include <set>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int k)
+    {
+        int ans = INT_MIN;
+        int m = matrix.size(), n = matrix[0].size();
+        for (int i = 0; i < m; ++i) { // 枚举上边界
+            vector<int> sum(n);
+            for (int j = i; j < m; ++j) { // 枚举下边界
+                for (int c = 0; c < n; ++c) {
+                    sum[c] += matrix[j][c]; // 更新每列的元素和
+                }
+                set<int> sumSet { 0 };
+                int s = 0;
+                for (int v : sum) {
+                    s += v;
+                    auto lb = sumSet.lower_bound(s - k);
+                    if (lb != sumSet.end()) {
+                        ans = max(ans, s - *lb);
+                    }
+                    sumSet.insert(s);
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// @lc code=end
