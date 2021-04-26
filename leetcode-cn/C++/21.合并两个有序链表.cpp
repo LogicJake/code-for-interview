cca297791711560a9703b2de8be1b9b594b31557
scy_lab <835410808@qq.com>
2021-04-26 15:59:23 +0800

    update

---
+++ leetcode-cn/C++/21.合并两个有序链表.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=21 lang=cpp
+ *
+ * [21] 合并两个有序链表
+ */
+
+// @lc code=start
+// struct ListNode {
+//     int val;
+//     ListNode* next;
+//     ListNode()
+//         : val(0)
+//         , next(nullptr)
+//     {
+//     }
+//     ListNode(int x)
+//         : val(x)
+//         , next(nullptr)
+//     {
+//     }
+//     ListNode(int x, ListNode* next)
+//         : val(x)
+//         , next(next)
+//     {
+//     }
+// };
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2)
+    {
+        ListNode* new_head = new ListNode();
+        ListNode* cur = new_head;
+        while (l1 != nullptr && l2 != nullptr) {
+            if (l1->val <= l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
+            }
+            cur = cur->next;
+        }
+
+        while (l1 != nullptr) {
+            cur->next = l1;
+            l1 = l1->next;
+            cur = cur->next;
+        }
+
+        while (l2 != nullptr) {
+            cur->next = l2;
+            l2 = l2->next;
+            cur = cur->next;
+        }
+
+        return new_head->next;
+    }
+};
+// @lc code=end
