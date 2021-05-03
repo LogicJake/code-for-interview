8967690ba6b780d22375d316f49cd4eeb5f25c1e
scy_laptop <835410808@qq.com>
2021-05-03 14:30:28 +0800

    update

---
+++ leetcode-cn/C++/31.下一个排列.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=cpp
+ *
+ * [31] 下一个排列
+ */
+
+// @lc code=start
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums)
+    {
+        int i = nums.size() - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if (i >= 0) {
+            int j = nums.size() - 1;
+            while (j >= 0 && nums[i] >= nums[j]) {
+                j--;
+            }
+            swap(nums[i], nums[j]);
+        }
+        reverse(nums.begin() + i + 1, nums.end());
+    }
+};
+
+// @lc code=end
