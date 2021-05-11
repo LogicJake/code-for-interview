b2b4ed29be8713952828d0635895490498185680
scy_laptop <835410808@qq.com>
2021-05-12 00:42:17 +0800

    update

--- leetcode-cn/C++/7.整数反转.cpp
+++ leetcode-cn/C++/7.整数反转.cpp
@@ -13,17 +13,21 @@ class Solution {
 public:
     int reverse(int x)
     {
-        long int ans = 0;
+        int ans = 0;
+
         while (x) {
             int pop = x % 10;
-            x = x / 10;
 
-            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && pop > 7))
+            if (ans > INT_MAX / 10 || (ans == INT_MAX / 10 && pop > 7)) {
                 return 0;
-            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && pop < -8))
+            }
+
+            if (ans < INT_MIN / 10 || (ans == INT_MIN / 10 && pop < -8)) {
                 return 0;
+            }
 
             ans = ans * 10 + pop;
+            x = x / 10;
         }
 
         return ans;
