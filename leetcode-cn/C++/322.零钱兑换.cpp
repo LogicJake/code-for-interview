1466163e04e0dea7639e68c10758de4813052cc2
scy_laptop <835410808@qq.com>
2021-05-17 09:59:33 +0800

    updateu

--- leetcode-cn/C++/322.零钱兑换.cpp
+++ leetcode-cn/C++/322.零钱兑换.cpp
@@ -13,19 +13,30 @@ class Solution {
 public:
     int coinChange(vector<int>& coins, int amount)
     {
-        vector<int> dp(amount + 1, amount + 1);
-        dp[0] = 0;
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(amount + 1, amount + 1));
 
-        for (int coin : coins) {
-            for (int i = 1; i <= amount; i++) {
-                if (coin <= i) {
-                    dp[i] = min(dp[i], dp[i - coin] + 1);
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 1; j <= amount; j++) {
+            if (j >= coins[0]) {
+                dp[0][j] = min(dp[0][j], dp[0][j - coins[0]] + 1);
+            }
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= amount; j++) {
+                if (j >= coins[i]) {
+                    dp[i][j] = min(dp[i - 1][j], dp[i][j - coins[i]] + 1);
+                } else {
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
 
-        if (dp.back() != amount + 1) {
-            return dp.back();
+        if (dp[n - 1][amount] != amount + 1) {
+            return dp[n - 1][amount];
         } else {
             return -1;
         }
