def3535107d62298c3cb610bc1954261fb3b8ae0
scy_laptop <835410808@qq.com>
2021-05-18 00:29:02 +0800

    update

---
+++ leetcode-cn/C++/1442.形成两个异或相等数组的三元组数目.cpp
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=1442 lang=cpp
+ *
+ * [1442] 形成两个异或相等数组的三元组数目
+ */
+
+// @lc code=start
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countTriplets(vector<int>& arr)
+    {
+        int n = arr.size();
+        vector<int> s(n + 1);
+        for (int i = 0; i < n; ++i) {
+            s[i + 1] = s[i] ^ arr[i];
+        }
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int k = i + 1; k < n; ++k) {
+                if (s[i] == s[k + 1]) {
+                    ans += k - i;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// @lc code=end
