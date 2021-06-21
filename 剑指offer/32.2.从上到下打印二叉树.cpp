16712c9278da8a0663d5e533a6adf624dbbc6354
scy_lab <835410808@qq.com>
2021-06-21 16:54:14 +0800

    update

--- 剑指offer/32.2.从上到下打印二叉树.cpp
+++ 剑指offer/32.2.从上到下打印二叉树.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <iostream>
 #include <vector>
 
@@ -19,5 +20,38 @@ class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root)
     {
+        vector<vector<int>> ans;
+
+        if (root == nullptr) {
+            return ans;
+        }
+
+        deque<TreeNode*> queue;
+
+        queue.push_back(root);
+
+        while (not queue.empty()) {
+            vector<int> tmp;
+
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                TreeNode* cur = queue.front();
+                queue.pop_front();
+
+                tmp.push_back(cur->val);
+
+                if (cur->left) {
+                    queue.push_back(cur->left);
+                }
+
+                if (cur->right) {
+                    queue.push_back(cur->right);
+                }
+            }
+
+            ans.push_back(tmp);
+        }
+
+        return ans;
     }
 };
