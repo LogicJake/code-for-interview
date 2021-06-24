0dcd28026af9c82ef0a3eeb8cd9343564aaacc35
scy_lab <835410808@qq.com>
2021-06-24 15:13:14 +0800

    update

---
+++ 剑指offer/53.1.在排序数组中查找数字 I.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target)
+    {
+        // 寻找出现target的第一个位置
+        int left = 0;
+        int right = nums.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                right = mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+
+        int first = left;
+
+        // 寻找出现target的最后一个位置
+
+        int left = 0;
+        int right = nums.size();
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid;
+            }
+        }
+
+        int last = left - 1;
+
+        return last - first + 1;
+    }
+};
