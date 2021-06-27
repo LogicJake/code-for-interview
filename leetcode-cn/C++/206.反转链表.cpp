04b9e159b19643e26ca6ded865186a890052695e
scy_laptop <835410808@qq.com>
2021-06-27 23:15:47 +0800

    update

---
+++ leetcode-cn/C++/206.反转链表.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=cpp
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode()
+        : val(0)
+        , next(nullptr)
+    {
+    }
+    ListNode(int x)
+        : val(x)
+        , next(nullptr)
+    {
+    }
+    ListNode(int x, ListNode* next)
+        : val(x)
+        , next(next)
+    {
+    }
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head)
+    {
+        if (head == nullptr || head->next = nullptr) {
+            return head;
+        }
+        auto new_head = reverseList(head->next);
+
+        head->next->next = head;
+        head->next = nullptr;
+
+        return new_head;
+    }
+};
+// @lc code=end
