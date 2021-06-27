8ef7e0c31c88f3d541da5cba60fc85ea2cd2e441
scy_laptop <835410808@qq.com>
2021-06-27 23:17:13 +0800

    update

--- leetcode-cn/C++/206.反转链表.cpp
+++ leetcode-cn/C++/206.反转链表.cpp
@@ -29,7 +29,7 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head)
     {
-        if (head == nullptr || head->next = nullptr) {
+        if (head == nullptr || head->next == nullptr) {
             return head;
         }
         auto new_head = reverseList(head->next);
