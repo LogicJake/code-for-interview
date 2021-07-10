aa948375c248a84ea3ba3fe3d9180a712c032a52
work <835410808@qq.com>
2021-07-10 22:10:08 +0800

    update

--- 程序员面试金典/17.23.最大黑方阵.cpp
+++ 程序员面试金典/17.23.最大黑方阵.cpp
@@ -4,43 +4,40 @@ class Solution {
 public:
     vector<int> findSquare(vector<vector<int>>& matrix)
     {
-        int m = matrix.size();
-        int n = matrix[0].size();
+        vector<int> ans;
+        int size = 0;
 
-        vector<vector<int>> dpx(m + 1, vector<int>(n + 1, 0));
-        vector<vector<int>> dpy(m + 1, vector<int>(n + 1, 0));
+        int n = matrix.size();
 
-        for (int i = m - 1; i >= 0; i--) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (matrix[i][j] == 0) {
-                    dpx[i][j] = dpx[i][j + 1] + 1;
-                    dpy[i][j] = dpy[i + 1][j] + 1;
+        if(n==0){
+            return {};
+        }
+
+        vector<vector<int>> dpx(n+1, vector<int>(n+1,0));
+        vector<vector<int>> dpy(n+1, vector<int>(n+1,0));
+
+        for(int i = n-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if (matrix[i][j] == 0){
+                    dpx[i][j] = dpx[i][j+1] + 1;
+                    dpy[i][j] = dpy[i+1][j] + 1;
                 }
             }
         }
 
-        vector<int> ans;
-        int width = 0;
-        // 枚举左上角的点
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                // 当前点构成的正方形的上边和左边的最短值
-                int cur = min(dpx[i][j], dpy[i][j]);
-
-                if (cur > width) {
-                    // 现在只能保证上边和左边长度为k，需要检查下边和右边
-                    // 检查下边就要检查 dpx[i+k-1][j]是否大于k
-                    // 检查右边就要检查 dpy[i][j+k-1]是否大于k
-                    for (int k = cur; k > width; k--) {
-                        if (dpx[i + k - 1][j] >= k && dpy[i][j + k - 1] >= k) {
-                            width = max(width, k);
-                            ans = { i, j, width };
-                            break;
-                        }
+        for(int i=0; i < n; i++){
+            for(int j =0;j<n;j++){
+                int width = min(dpx[i][j], dpy[i][j]);
+                for(int c = width; c>size; c--){
+                    if(dpx[i+c-1][j]>=c && dpy[i][j+c-1]>=c){
+                        size = c;
+                        ans = {i, j, size};
+                        break;
                     }
                 }
             }
         }
+
 
         return ans;
     }
