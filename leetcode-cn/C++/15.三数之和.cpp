dfbc9f5b9d7727d46d21f3866d25195adc4adf7b
work <835410808@qq.com>
2021-08-17 18:02:23 +0800

    update

--- leetcode-cn/C++/15.三数之和.cpp
+++ leetcode-cn/C++/15.三数之和.cpp
@@ -10,36 +10,48 @@
 #include <vector>
 
 using namespace std;
-class Solution {
+class Solution
+{
 public:
-    vector<vector<int>> threeSum(vector<int>& nums)
+    vector<vector<int>> threeSum(vector<int> &nums)
     {
-        vector<vector<int>> ans;
-        sort(nums.begin(), nums.end());
         int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
 
-        for (int first = 0; first < n - 2; first++) {
-            if (first > 0 && nums[first] == nums[first - 1]) {
+        for (int i = 0; i < n; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1])
+            {
                 continue;
             }
 
-            int second = first + 1;
-            int third = n - 1;
+            int j = i + 1;
+            int k = n - 1;
 
-            while (second < third) {
-                if (second > first + 1 && nums[second] == nums[second - 1]) {
-                    second++;
-                    continue;
+            while (j < k)
+            {
+                if (nums[i] + nums[j] + nums[k] < 0)
+                {
+                    j++;
                 }
-
-                if (nums[first] + nums[second] + nums[third] < 0) {
-                    second++;
-                } else if (nums[first] + nums[second] + nums[third] > 0) {
-                    third--;
-                } else {
-                    ans.push_back({ nums[first], nums[second], nums[third] });
-                    second++;
-                    third--;
+                else if (nums[i] + nums[j] + nums[k] > 0)
+                {
+                    k--;
+                }
+                else
+                {
+                    ans.push_back({nums[i], nums[j], nums[k]});
+                    while (j < k && nums[j] == nums[j + 1])
+                    {
+                        j++;
+                    }
+                    j++;
+                    while (j < k && nums[k] == nums[k - 1])
+                    {
+                        k--;
+                    }
+                    k--;
                 }
             }
         }
