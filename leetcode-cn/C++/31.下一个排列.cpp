ee48dc48f96fde5f8f2aedb9820275904dc5c894
work <835410808@qq.com>
2021-08-21 14:40:11 +0800

    update

--- leetcode-cn/C++/31.下一个排列.cpp
+++ leetcode-cn/C++/31.下一个排列.cpp
@@ -10,22 +10,43 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    void nextPermutation(vector<int>& nums)
+    void nextPermutation(vector<int> &nums)
     {
-        int i = nums.size() - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
+        int n = nums.size();
+
+        if (n <= 1)
+        {
+            return;
         }
-        if (i >= 0) {
-            int j = nums.size() - 1;
-            while (j >= 0 && nums[i] >= nums[j]) {
-                j--;
+
+        int i = n - 2;
+        for (; i >= 0; i--)
+        {
+            if (nums[i] < nums[i + 1])
+            {
+
+                break;
             }
-            swap(nums[i], nums[j]);
         }
-        reverse(nums.begin() + i + 1, nums.end());
+
+        if (i >= 0)
+        {
+            for (int j = n - 1; j >= i + 1; j--)
+            {
+                if (nums[j] > nums[i])
+                {
+                    int temp = nums[j];
+                    nums[j] = nums[i];
+                    nums[i] = temp;
+                    break;
+                }
+            }
+        }
+
+        sort(nums.begin() + i + 1, nums.end());
     }
 };
 
