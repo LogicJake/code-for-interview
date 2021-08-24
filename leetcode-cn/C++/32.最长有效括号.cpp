5f460342160ce17856655f126abe78da34adc2a0
work <835410808@qq.com>
2021-08-24 23:06:28 +0800

    update

--- leetcode-cn/C++/32.最长有效括号.cpp
+++ leetcode-cn/C++/32.最长有效括号.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
     int longestValidParentheses(string s)
     {
@@ -18,27 +19,43 @@ public:
         int ans = 0;
 
         vector<int> dp(n, 0);
-        for (int i = 1; i < n; i++) {
-            if (s[i] == ')') {
-                if (s[i - 1] == '(') {
-                    if (i - 2 >= 0) {
-                        dp[i] = dp[i - 2] + 2;
-                    } else {
-                        dp[i] = 2;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == ')')
+            {
+                if (i - 1 >= 0)
+                {
+                    if (s[i - 1] == '(')
+                    {
+                        if (i - 2 >= 0)
+                        {
+                            dp[i] = dp[i - 2] + 2;
+                        }
+                        else
+                        {
+                            dp[i] = 2;
+                        }
                     }
-                } else {
-                    if (i - 1 - dp[i - 1] >= 0 && s[i - 1 - dp[i - 1]] == '(') {
-                        if (i - 2 - dp[i - 1] > 0) {
-                            dp[i] = dp[i - 1] + dp[i - 2 - dp[i - 1]] + 2;
-                        } else {
-                            dp[i] = dp[i - 1] + 2;
+                    else
+                    {
+                        if (i - dp[i - 1] - 1 >= 0 && s[i - dp[i - 1] - 1] == '(')
+                        {
+                            if (i - dp[i - 1] - 2 >= 0)
+                            {
+                                dp[i] = dp[i - dp[i - 1] - 2] + 2 + dp[i - 1];
+                            }
+                            else
+                            {
+                                dp[i] = 2 + dp[i - 1];
+                            }
                         }
                     }
                 }
             }
-
             ans = max(ans, dp[i]);
         }
+
         return ans;
     }
 };
