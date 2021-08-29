a8ead812e01fd834093c88ae784a6d8836127483
work <835410808@qq.com>
2021-08-29 20:07:48 +0800

    update

--- leetcode-cn/C++/5.最长回文子串.cpp
+++ leetcode-cn/C++/5.最长回文子串.cpp
@@ -6,38 +6,50 @@
 
 // @lc code=start
 #include <string>
+#include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    pair<int, int> help(const string s, int left, int right)
-    {
-        while (left >= 0 && right < s.size() && s[left] == s[right]) {
-            left -= 1;
-            right += 1;
-        }
-        left++;
-        right--;
-        return { left, right };
-    }
-
     string longestPalindrome(string s)
     {
+        int n = s.size();
+        vector<vector<bool>> dp(n, vector<bool>(n));
         int start = 0;
         int end = 0;
 
-        for (int i = 0; i < s.size(); i++) {
-            auto [left1, right1] = help(s, i, i);
-            auto [left2, right2] = help(s, i, i + 1);
+        for (int len = 1; len <= s.size(); len++)
+        {
+            for (int i = 0; i < s.size(); i++)
+            {
+                int j = i + len - 1;
+                if (j >= s.size())
+                {
+                    break;
+                }
 
-            if (right1 - left1 > end - start) {
-                start = left1;
-                end = right1;
-            }
+                if (s[i] == s[j])
+                {
+                    if (j - i < 3)
+                    {
+                        dp[i][j] = true;
+                    }
+                    else
+                    {
+                        dp[i][j] = dp[i + 1][j - 1];
+                    }
+                }
+                else
+                {
+                    dp[i][j] = false;
+                }
 
-            if (right2 - left2 > end - start) {
-                start = left2;
-                end = right2;
+                if (dp[i][j] && j - i > end - start)
+                {
+                    start = i;
+                    end = j;
+                }
             }
         }
         return s.substr(start, end - start + 1);
