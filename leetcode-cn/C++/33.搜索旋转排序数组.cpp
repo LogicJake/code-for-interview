83bfca0047c290744f491251322b4e0a8054d474
work <835410808@qq.com>
2021-08-29 20:25:48 +0800

    update

--- leetcode-cn/C++/33.搜索旋转排序数组.cpp
+++ leetcode-cn/C++/33.搜索旋转排序数组.cpp
@@ -12,21 +12,20 @@ class Solution
 public:
     int search(vector<int> &nums, int target)
     {
-        int n = nums.size();
         int left = 0;
-        int right = n - 1;
+        int right = nums.size() - 1;
 
         while (left <= right)
         {
             int mid = (left + right) / 2;
-
             if (nums[mid] == target)
             {
                 return mid;
             }
+
             else if (nums[mid] >= nums[0])
             {
-                if (target >= nums[0] && target < nums[mid])
+                if (nums[left] <= target && target < nums[mid])
                 {
                     right = mid - 1;
                 }
@@ -37,7 +36,7 @@ public:
             }
             else
             {
-                if (target > nums[mid] && target <= nums[right])
+                if (nums[mid] < target && target <= nums[right])
                 {
                     left = mid + 1;
                 }
