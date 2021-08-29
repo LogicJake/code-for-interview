d94a726daf1904619adf3df101d112951a9c6f09
work <835410808@qq.com>
2021-08-29 14:52:49 +0800

    update

--- leetcode-cn/C++/31.下一个排列.cpp
+++ leetcode-cn/C++/31.下一个排列.cpp
@@ -16,35 +16,37 @@ public:
     void nextPermutation(vector<int> &nums)
     {
         int n = nums.size();
-
-        if (n <= 1)
+        if (n == 1)
         {
             return;
         }
 
         int i = n - 2;
-        for (; i >= 0; i--)
+
+        while (i >= 0)
         {
             if (nums[i] < nums[i + 1])
             {
-
                 break;
             }
+            i--;
         }
 
-        if (i >= 0)
+        if (i < 0)
         {
-            for (int j = n - 1; j >= i + 1; j--)
-            {
-                if (nums[j] > nums[i])
-                {
-                    int temp = nums[j];
-                    nums[j] = nums[i];
-                    nums[i] = temp;
-                    break;
-                }
-            }
+            sort(nums.begin(), nums.end());
+            return;
         }
+
+        int j = n - 1;
+        while (nums[j] <= nums[i])
+        {
+            j--;
+        }
+
+        int temp = nums[j];
+        nums[j] = nums[i];
+        nums[i] = temp;
 
         sort(nums.begin() + i + 1, nums.end());
     }
