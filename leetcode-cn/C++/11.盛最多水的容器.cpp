bf5b585e93a3ad3197a4765eca3a326ed992611b
work <835410808@qq.com>
2021-09-04 18:58:21 +0800

    update

--- leetcode-cn/C++/11.盛最多水的容器.cpp
+++ leetcode-cn/C++/11.盛最多水的容器.cpp
@@ -8,22 +8,29 @@
 #include <vector>
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int maxArea(vector<int>& height)
+    int maxArea(vector<int> &height)
     {
         int left = 0;
         int right = height.size() - 1;
-
         int ans = 0;
-        while (left < right) {
+
+        while (left < right)
+        {
             ans = max(ans, min(height[left], height[right]) * (right - left));
-            if (height[left] > height[right]) {
-                right--;
-            } else {
+
+            if (height[left] < height[right])
+            {
                 left++;
             }
+            else
+            {
+                right--;
+            }
         }
+
         return ans;
     }
 };
